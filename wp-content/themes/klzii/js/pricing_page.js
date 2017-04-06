83e176fd7119660af98c83907648edd4bec7ad5f
warlockxins <alexander.semionov@diatomenterprises.com>
2017-04-06 11:13:31 +0300

    ordering for plans

--- wp-content/themes/klzii/js/pricing_page.js
+++ wp-content/themes/klzii/js/pricing_page.js
@@ -24,9 +24,22 @@ function displayPrices(data) {
 function planWithCurrency(plans, currency) {
   if (!plans || !currency) {
     return {};
+  }
+
+  var currencyPlanSelected = plans[currency];
+  var planSelected;
+  if (!currencyPlanSelected) {
     planSelected = [];
   } else {
+    planSelected = currencyPlanSelected.month || [];
   }
+
+  var orderedPlans = _.map(planOrder, function(p) {
+    return _.find(planSelected, function(subPlan) {
+      return _.includes(subPlan.plan.id, p);
+    });
+  });
+  return orderedPlans;
 }
 
 function mapPlans(plans) {
@@ -42,7 +55,7 @@ function mapPlans(plans) {
   });
 
   supportedCurrencies.forEach(function(currency) {
-    plans[currency] = _.groupBy(plans[currency], (item) => item.plan.period_unit); // group by period
+    plans[currency] = _.groupBy(plans[currency], (item) => item.plan.period_unit);
   });
   return plans;
 }
